b03e25a013c8896c60ae83af6e96e8251116f9e3
Sabrina Goldfarb <s.goldfarb2@gmail.com>
2019-04-17 14:08:24 -0500

    working through list exercises and string exercises

---
+++ listexercises.py
@@ -0,0 +1,85 @@
+#1
+# list_of_nums = [1, 2, 3, 4, 5, 6]
+# our_sum = 0
+# for num in list_of_nums:
+#   our_sum = our_sum + num
+# print(our_sum)
+
+# 2
+# list_of_nums = [1, 2, 3, 4, 5, 1, 16, 4, 6]
+# current_largest = list_of_nums[0]
+
+# for number in list_of_nums:
+#   if(number > current_largest):
+#     current_largest = number
+
+# print(current_largest)
+
+#3
+# list_of_nums = [1, 2, 4, 6, 34, 0, 3, 6, -1, 2]
+# current_smallest = list_of_nums[0]
+
+# for number in list_of_nums:
+#   if(number < current_smallest):
+#     current_smallest = number
+# print(current_smallest)
+
+
+#4
+# list_of_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# for number in list_of_nums:
+#   if(number % 2 == 0):
+#     print(number)
+
+
+#5
+# list_of_nums = [-20, 5, -45, 8, 0, 1]
+
+# for number in list_of_nums:
+#   if(number > 0):
+#     print(number)
+
+
+#6
+# list_of_nums = [-12, 2, 12, -7845, 1, 5]
+# new_list = []
+
+# for number in list_of_nums:
+#   if(number > 0):
+#     new_list.append(number)
+# print(new_list)
+
+
+#7
+# list_of_nums = [1, 2, 3, 4, 5]
+# factor_to_multiply_by = 2
+# multiplied_nums = []
+
+# for number in list_of_nums:
+#   multiplied_nums.append(number * factor_to_multiply_by)
+
+# print(multiplied_nums)
+
+
+#8
+# list_one = [2, 4, 5]
+# list_two = [2, 3, 6]
+#work from here on...
+
+
+
+
+
+#11
+# list_of_stuff = [1, 2, 1, 2, 4, 5, 6, 5, 7, 8, 8]
+# list_of_stuff.sort()
+# new_list = []
+# previous_item = list_of_stuff[-1]
+#in line 78, since the items have been sorted, the last item should never be the same as the first item, so this should work
+# for item in list_of_stuff:
+#   if(item != previous_item):
+#     new_list.append(item)
+#   previous_item = item
+# print(new_list)
+
---
+++ stringexercises.py
@@ -0,0 +1,41 @@
+# 1
+# str = 'my lowercase string'
+# str = str.upper()
+# print(str)
+
+# 2
+# same as number 1?
+
+# 3
+# str = 'whoa Im going to be reversed'
+# reversed_str = str[::-1]
+# print(reversed_str)
+
+# 4
+# leet_array = ['O', 'I', 'L', 'E', 'A', 'S', 'G', 'T']
+# str_to_translate = 'LEET'
+# translated_list = []
+
+# for letter in str_to_translate:
+#     translated_list.append(str(leet_array.index(letter)))
+
+# end = ''.join(translated_list)
+# print(end)
+
+
+# 5
+# word = 'sweet string'
+# letter_to_duplicate = ''
+# previous_letter = ''
+# for letter in word:
+#     if (letter == previous_letter):
+#         letter_to_duplicate = letter
+
+#     previous_letter = letter
+
+# index_to_insert_letter = word.index(letter_to_duplicate)
+# skipping the rest of this one for now...got to the index of the letter we need to repeat, now have to add that in to the string
+
+
+# 6
+# will try later. maybe use slicing to move our alphabet 13 letters and then use indexes like for leet to translate
