d2fc8dec6ea2c5bdbb301e43ceb72366f3dce16b
Sabrina Goldfarb <s.goldfarb2@gmail.com>
2019-04-17 14:20:01 -0500

    working through loop exercises

---
+++ loopexercises.py
@@ -0,0 +1,22 @@
+#1
+# for number in range(1, 11):
+#   print(number)
+
+#2
+# n = int(input('Where should I start? '))
+# m = int(input('Where should I end? '))
+
+# for number in range(n, (m+1)):
+#   print(number)
+
+#3
+# for number in range(1, 10, 2):
+#   print(number)
+
+#OR
+# for number in range(1, 10):
+#   if(number % 2 == 1):
+#     print(number)
+
+
+#4
